1489b8ab4ee6e656b8488fa91063576b5adfc67a
JFlores427 <Flores.Josue427@gmail.com>
2023-09-26 15:16:38 -0400

    Added PokeDex and Fixed Styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import Flashcard from './components/Flashcard.jsx';
 import './App.css';
 
 function App() {
-
-
-
   return (
     <div className="App">
       <h1>Guess That Pokemon</h1>
       <p className="description">Can You Guess and "Catch" Them All?</p>
-      <p className="cardCount">Number of Cards: {PokemonDict.length}</p>
+      <p className="card-count">Number of Cards: {PokemonDict.length}</p>
       <Flashcard cardSet={PokemonDict} />
     
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -5,26 +5,32 @@ const Flashcard = (props) => {
 
     const [index, setIndex] = useState({current: 0, previous: 0});
 
+
     const handleToggle = () => {
         const question = document.getElementById("question");
         const answer = document.getElementById("answer");
 
         question.classList.toggle("hidden");
         answer.classList.toggle("hidden");
-        // image.classList.toggle("hidden");
     }
 
     const handlePrevious = () => {
         setIndex({...index, current: index.previous});
+        
+        const question = document.getElementById("question");
+        const answer = document.getElementById("answer");
+        
+        question.classList.remove("hidden");
+        answer.classList.add("hidden");
     }
 
     const handleNext = () => {
         const randomCard = getRandomInt(props.cardSet.length);
-        setIndex({...index, previous: index.current, current: randomCard });
-        
         const question = document.getElementById("question");
         const answer = document.getElementById("answer");
 
+        setIndex({...index, previous: index.current, current: randomCard });
+        
         question.classList.remove("hidden");
         answer.classList.add("hidden");
     }
@@ -35,27 +41,25 @@ const Flashcard = (props) => {
     }
 
     return (
-        <>
-        <div className={"container " + props.cardSet[index.current].difficulty} onClick ={handleToggle}>
-            <img className="image" id="image" src={props.cardSet[index.current].image}></img>
-            <img src={props.cardSet[index.current].footprint} className="footprint"></img>
-            
+    <div> 
+        <div className='pokedex'>
+            {/* <img className="pokedex" src="../src/assets/Kalos_Pokedex.jpg"></img>                         */}
+            <div className={"container " + props.cardSet[index.current].difficulty} onClick ={handleToggle}>
+            <img className="image" src={props.cardSet[index.current].image}></img>
+            <img className="footprint" src={props.cardSet[index.current].footprint}></img>
             <div className="details">
                 <div className="height">{props.cardSet[index.current].height}</div>
                 <div className="weight">{props.cardSet[index.current].weight}</div>
             </div>
             <div className="question" id="question">???</div>
             <div className="answer hidden" id="answer">{props.cardSet[index.current].answer}</div>
-            
-           
-         
-
             <div className="entry-description" id="entry-description">{props.cardSet[index.current].description}</div>
-        </div>
+            </div>
 
-        <img src='../src/assets/PokeBall_Left.gif' className="previous" onClick={handlePrevious}></img>
-        <img src='../src/assets/GreatBall_Right.gif' className="next" onClick={handleNext}></img> 
-        </>
+            <img src='../src/assets/PokeBall_Left.gif' className="previous" onClick={handlePrevious}></img>
+            <img src='../src/assets/GreatBall_Right.gif' className="next" onClick={handleNext}></img> 
+            </div>
+        </div>
     )
 }
 
