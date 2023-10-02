92383088dcbf49852b1c2af5d57f83d0e25dc534
JFlores427 <Flores.Josue427@gmail.com>
2023-10-02 03:35:40 -0400

    Week 2 Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 import PokemonDict from "./pokemonDict";
 import Flashcard from './components/Flashcard.jsx'; 
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [streak, setStreak] = useState({current: 0, longest: 0});
+
+  function streakUpdate(e) {
+
+    console.log("hello");
+    const input = document.getElementById('input');
+    if (input.classList.contains('correct')) {
+      setStreak({...streak, longest: (streak.current + 1 > streak.longest) ? streak.current : streak.longest, current: streak.current + 1})
+      return;
+    }
+    
+    if (input.classList.contains('wrong')) {
+      setStreak({...streak, current: 0})
+    } 
+  }
+
   return (
     <div className="App">
       <h1>Guess That Pokemon</h1>
       <p className="description">Can You Guess and "Catch" Them All?</p>
       <p className="card-count">Number of Cards: {PokemonDict.length}</p>
-      <Flashcard cardSet={PokemonDict} />
-    
+      <p className="current-streak"> Current Streak: {streak.current} Longest Streak: {streak.longest} </p>
+      <Flashcard cardSet={PokemonDict} streakUpdate={streakUpdate} />
+
     </div>
   )
 }
--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -2,29 +2,45 @@ import { useState } from "react";
 import "./Flashcard.css";
 
 const Flashcard = (props) => {
-    const [index, setIndex] = useState({current: 0, previous: 0});
+    const [index, setIndex] = useState({ current: 0, previous: 0});
 
     const handleToggle = () => {
         const flipCard = document.getElementById("flip-card");
-        
-        flipCard.classList.toggle("flip");
+        const input = document.getElementById('guess');
+
+        if (input.classList.contains('correct') || input.classList.contains('wrong')) {
+            flipCard.classList.toggle("flip");
+        }
+
     }
 
     const handlePrevious = () => {
+        let prev = index.current;
+        let curr = index.current - 1;
         
-        const flipCard = document.getElementById("flip-card");
-        
-        flipCard.classList.remove("flip");
+        if (curr < 0) {
+            curr = props.cardSet.length - 1;
+        } 
 
-        setTimeout(() => { setIndex({...index, current: index.previous})}, 100);
+        const flipCard = document.getElementById("flip-card");
+        const input = document.getElementById('guess');
+
+        flipCard.classList.remove("flip");
+        input.classList.remove('wrong');
+        input.classList.remove('correct');
+
+        setTimeout(() => { setIndex({...index, current: curr, previous: prev})}, 100);
     }
+    
     const handleNext = () => {
-        const randomCard = getRandomInt(props.cardSet.length);
         const flipCard = document.getElementById("flip-card");
-        
-        flipCard.classList.remove("flip");
+        const input = document.getElementById('guess');
 
-        setTimeout(() => {setIndex({...index, previous: index.current, current: randomCard })}, 100);
+        flipCard.classList.remove("flip");
+        input.classList.remove('wrong');
+        input.classList.remove('correct');
+
+        setTimeout(() => {setIndex({...index, previous: index.current, current: (index.current + 1) % props.cardSet.length })}, 100);
     }
 
     const getRandomInt = (max) => {
@@ -32,9 +48,72 @@ const Flashcard = (props) => {
         return (randomValue !== max) ? randomValue : getRandomInt(max);
     }
 
+    const handleMaster = () => {
+
+    };
+
+    const handleShuffle = () => {
+        const flipCard = document.getElementById('flip-card');
+        const input = document.getElementById('guess');
+
+        if (flipCard.classList.contains('flip')) {
+             flipCard.classList.toggle('flip');
+        }
+
+        input.classList.remove('wrong');
+        input.classList.remove('correct');
+
+        for(let i = 0; i < 10; i++) {
+            const random1 = getRandomInt(props.cardSet.length);
+            const random2 = getRandomInt(props.cardSet.length);
+            
+            const temp = props.cardSet[random1];
+            props.cardSet[random1] = props.cardSet[random2];
+            props.cardSet[random2] = temp;
+        }
+
+        setTimeout(() => { setIndex({...index, current: 0, previous: props.cardSet.length - 1}) }, 0);
+    }
+
+    const onCheckAnswer = (e) => {
+        e.preventDefault();
+        
+        const input = document.getElementById('guess');
+        let errors = 0;
+
+        const correctAnswer = props.cardSet[index.current].answer;
+        const guessedAnswer = input.value.trim();
+
+        if(Math.abs(guessedAnswer.length - correctAnswer.length) > 1) {
+            input.classList.remove('correct');
+            input.classList.add('wrong');
+            props.streakUpdate();
+            return;
+        }
+
+        for (let i = 0; i < Math.min(correctAnswer.length, guessedAnswer.length); i++) {
+            if(errors > 1) {
+                input.classList.remove('correct');
+                input.classList.add('wrong');
+                props.streakUpdate();
+                return;
+            }
+
+            if (guessedAnswer.charAt(i) !== correctAnswer.charAt(i)) {
+                errors++;
+                continue;
+            }
+        }
+            
+        input.classList.remove('wrong');
+        input.classList.add("correct");
+        props.streakUpdate();
+    }
+
     return (
         <div> 
             <div className='pokedex'>
+                <img src='../src/assets/SpinningPokeBall.gif' className="shuffle" onClick={handleShuffle}></img> 
                 <div className="flip-card" id="flip-card">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
@@ -63,8 +142,21 @@ const Flashcard = (props) => {
                         </div>
                     </div>
                 </div>
-                <img src='../src/assets/PokeBall_Left.gif' className="previous" onClick={handlePrevious}></img>
-                <img src='../src/assets/GreatBall_Right.gif' className="next" onClick={handleNext}></img> 
+                                
+
+                <img src="../src/assets/MasterBall.gif" className="master" onClick={handleMaster}></img>
+
+                <div className="button-container">
+                    <img src='../src/assets/PokeBall_Left.gif' className="previous" onClick={handlePrevious}></img>
+                    <img src='../src/assets/GreatBall_Right.gif' className="next" onClick={handleNext}></img> 
+                </div>
+                
+                <form className="form-container" id="pokemon-form">
+                    <label for='guess'>Pokemon:</label>
+                    <input type="text" id="guess" placeholder="Name of Pokemon"></input>
+                    <button type="submit" onClick={(event) => { onCheckAnswer(event); streakUpdate(event); }} >Check Guess</button>
+                </form>
+
             </div>
         </div>
     );
